1a169a0b25e7fe936d66cf972a8df06457762547
Sebastian Janzen <sja@sja-mbp.home.janzen.it>
2017-01-15 23:03:13 +0100

    Add test for websocket
    The test opens a WebSocket server on a random port on localhost and configures the pilight adapter to connect to it. It ensures, that it receives a `request values` action.
    
    Signed-off-by: Sebastian Janzen <sja@sja-mbp.home.janzen.it>

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -9,6 +9,21 @@ var onStateChanged = null;
 var onObjectChanged = null;
 var sendToID = 1;
 
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+let randomWebsocketPort;
+const mockHttpServer = http
+    .createServer((req, res) => {
+        console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>GOT REQ HERE!');
+        res.end();
+    })
+    .listen(0, 'localhost', (err) => {
+        if (err) console.error(err);
+        randomWebsocketPort = mockHttpServer.address().port;
+        console.log('Started Mock HTTP Server on random port ' + randomWebsocketPort);
+    });
+const wsMockServer = new WebSocketServer({httpServer: mockHttpServer});
+
 var adapterShortName = setup.adapterName.substring(setup.adapterName.indexOf('.')+1);
 
 function checkConnectionOfAdapter(cb, counter) {
@@ -74,6 +89,30 @@ function sendTo(target, command, message, callback) {
 }
 
 describe('Test ' + adapterShortName + ' adapter', function() {
+    let lastWsConnection = null;
+    let lastWsMessage = null;
+
+    wsMockServer.on('request', (req) => {
+        var connection = req.accept(null, req.origin);
+
+        if (lastWsConnection != null) {
+            console.log('Connection closed by test.', lastWsConnection);
+            lastWsConnection.close();
+        }
+        lastWsConnection = connection;
+
+        lastWsConnection.on('message', (msg) => {
+            if (msg.type === 'utf8') {
+                lastWsMessage = msg.utf8Data;
+            }
+        });
+
+        connection.on('close', function(connection) {
+            console.log('Connection closed.');
+        });
+
+    });
+
     before('Test ' + adapterShortName + ' adapter: Start js-controller', function (_done) {
         this.timeout(600000); // because of first install from npm
 
@@ -83,14 +122,14 @@ describe('Test ' + adapterShortName + ' adapter', function() {
             config.common.enabled  = true;
             config.common.loglevel = 'debug';
 
-            //config.native.dbtype   = 'sqlite';
+            config.native.port = randomWebsocketPort;
 
             setup.setAdapterConfig(config.common, config.native);
 
-            setup.startController(true, function(id, obj) {}, function (id, state) {
+            setup.startController(true, function objChange(id, obj) {}, function stateChange(id, state) {
                     if (onStateChanged) onStateChanged(id, state);
                 },
-                function (_objects, _states) {
+                function cb(_objects, _states) {
                     objects = _objects;
                     states  = _states;
                     _done();
@@ -98,9 +137,6 @@ describe('Test ' + adapterShortName + ' adapter', function() {
         });
     });
 
-/*
-    ENABLE THIS WHEN ADAPTER RUNS IN DEAMON MODE TO CHECK THAT IT HAS STARTED SUCCESSFULLY
-*/
     it('Test ' + adapterShortName + ' adapter: Check if adapter started', function (done) {
         this.timeout(60000);
         checkConnectionOfAdapter(function (res) {
@@ -118,23 +154,25 @@ describe('Test ' + adapterShortName + ' adapter', function() {
                 });
         });
     });
-/**/
-
-/*
-    PUT YOUR OWN TESTS HERE USING
-    it('Testname', function ( done) {
-        ...
+    
+    it(`Test ${adapterShortName} adapter: Check for established websocket connection`, function () {
+        expect(lastWsConnection).to.not.be.null;
+        expect(lastWsMessage).to.be.equal('{"action":"request values"}');
+        lastWsConnection.send('{}');
     });
-
-    You can also use "sendTo" method to send messages to the started adapter
-*/
 
     after('Test ' + adapterShortName + ' adapter: Stop js-controller', function (done) {
         this.timeout(10000);
 
         setup.stopController(function (normalTerminated) {
             console.log('Adapter normal terminated: ' + normalTerminated);
-            done();
+            if (lastWsConnection) {
+                lastWsConnection.close();
+            }
+            wsMockServer.shutDown();
+            mockHttpServer.close(() => done());
         });
+
     });
+
 });
